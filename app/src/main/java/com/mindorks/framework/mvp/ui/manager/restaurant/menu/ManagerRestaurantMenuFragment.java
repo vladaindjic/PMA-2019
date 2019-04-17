3c3b517c32759ad38f95350104935d354b0c6a73
vladaindjic <vladaindjic@icloud.com>
2019-04-17 22:05:06 +0200

    Trebalo bi da je update menija priveden kraju. Odradjeno dodavanje disType-a, kao i submit i cancel.

--- app/src/main/java/com/mindorks/framework/mvp/ui/manager/restaurant/menu/ManagerRestaurantMenuFragment.java
+++ app/src/main/java/com/mindorks/framework/mvp/ui/manager/restaurant/menu/ManagerRestaurantMenuFragment.java
@@ -6,12 +6,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.mindorks.framework.mvp.R;
 import com.mindorks.framework.mvp.data.network.model.MenuResponse;
@@ -33,6 +34,11 @@ public class ManagerRestaurantMenuFragment extends BaseFragment implements Manag
         ManagerDishTypeListAdapter.ManagerDishTypeItemListCallback,
         ManagerDishListAdapter.ManagerDishListItemCallback {
 
+    private static Long counter = 0L;
+    synchronized private static Long getNewCounter() {
+        return --counter;
+    }
+
     private static final String TAG = "ManagerRestaurantMenuFragment";
 
     @Inject
@@ -49,6 +55,18 @@ public class ManagerRestaurantMenuFragment extends BaseFragment implements Manag
 
     @BindView(R.id.manager_edit_text_menu_name)
     EditText editMenuName;
+
+    @BindView(R.id.manager_menu_dish_type_edit_text)
+    EditText editDishTypeName;
+
+    @BindView(R.id.manager_menu_add_dish_type_btn)
+    Button btnAddDishType;
+
+    @BindView(R.id.manager_menu_submit_btn)
+    Button btnSubmit;
+    @BindView(R.id.manager_menu_cancel_btn)
+    Button btnCancel;
+
 
     private MenuResponse.Menu menu;
     private MenuResponse.Menu originalMenu;
@@ -94,6 +112,50 @@ public class ManagerRestaurantMenuFragment extends BaseFragment implements Manag
         // TODO vi3: ovde svakako ide restoran ciji je korisnik manager
         mPresenter.onViewPrepared(1L);
         mPresenter.getAllRestaurantDishes(1L);
+
+        // na klik dugmeta radimo dodavanje
+        btnAddDishType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewDishTypeToMenu();
+            }
+        });
+
+        editDishTypeName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                checkIfDishTypeNameIsOkAndAllowBtnAddClick();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkIfDishTypeNameIsOkAndAllowBtnAddClick();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkIfDishTypeNameIsOkAndAllowBtnAddClick();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitUpdatingMenu();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelUpdatingMenu();
+            }
+        });
+
+
+        // proverimo da li treba da se dozvoli klik ili ne
+        // podrazumevano bi trebalo da bude blokirano
+        checkIfDishTypeNameIsOkAndAllowBtnAddClick();
     }
 
     // updateMenu ili updateAllRestaurantDishes mora da se zavrsi ranije
@@ -181,5 +243,69 @@ public class ManagerRestaurantMenuFragment extends BaseFragment implements Manag
         // trebalo bi da update dishtype liste odradi i update dish lista
         // ali i autocomplete-ova
         mDishTypeListAdapter.addItems(this.menu.getDishTypeList());
+    }
+
+    public void checkIfDishTypeNameIsOkAndAllowBtnAddClick() {
+        // nema edit text, pa blokiramo dodavanje
+        if (editDishTypeName == null) {
+            btnAddDishType.setClickable(false);
+            return;
+        }
+
+        // prazan unos, pa blokiramo dodavanje
+        String typedTxt = editDishTypeName.getText().toString().trim();
+        if (typedTxt.equals("")) {
+            btnAddDishType.setClickable(false);
+            return;
+        }
+
+        // da li se mozda neko ime poklapa?
+        for (MenuResponse.DishType dt: this.menu.getDishTypeList()) {
+            if (dt.getName().toUpperCase().equals(typedTxt.toUpperCase())) {
+                btnAddDishType.setClickable(false);
+                return;
+            }
+        }
+
+        // unos je u redu, moze dopustiti dodavanje
+        btnAddDishType.setClickable(true);
+    }
+
+    public void addNewDishTypeToMenu() {
+        MenuResponse.DishType dishType = new MenuResponse.DishType();
+        // Ako ne radim direktan submit, nemam id
+        // Pa cu ubaciti svoj reverse counter da dobijam unique negativne identifikatore.
+        // Pre nego sto odradim submit, svima setujem id na null, pa ce biti kreirani na serveru.
+        dishType.setId(getNewCounter());
+        // ime preuzimamo iz edit texta
+        dishType.setName(editDishTypeName.getText().toString());
+        // nemamo jela
+        dishType.setDishList(new ArrayList<MenuResponse.Dish>());
+        // dodajemo dish type na postojecu listu u meniju
+        this.menu.getDishTypeList().add(dishType);
+        // update-ujemo samo listu u meniju
+        this.mDishTypeListAdapter.addItems(this.menu.getDishTypeList());
+        // ponistavamo unos
+        this.editDishTypeName.setText("");
+    }
+
+    private void submitUpdatingMenu() {
+        MenuResponse.Menu menu = new MenuResponse.Menu();
+        menu.setId(this.menu.getId());
+        menu.setName(this.menu.getName());
+        // proveriti da li treba da se pravi nova lista
+        menu.setDishTypeList(this.menu.getDishTypeList());
+
+        mPresenter.submitMenu(menu);
+
+        // TODO vi3: takodje treba videti koji je najbolji nacin da se uradi
+        // update fotografije
+    }
+
+    private void cancelUpdatingMenu() {
+        // posto smo memoizovali restoran koji smo svukli sa neta
+        // samo ih ponovo postavimo i to je to
+        // sve izmene ce biti ponostene
+        this.updateMenu(this.originalMenu);
     }
 }
